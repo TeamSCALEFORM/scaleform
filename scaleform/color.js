60bebd3377be2117e5a9013f2bc850c13bc7ffda
cristei <51248378+cristeigabriel@users.noreply.github.com>
2022-07-24 00:30:30 +0300

    radar

--- scaleform/color.js
+++ scaleform/color.js
@@ -7,12 +7,14 @@ roundBorder.style.borderRadius = '50% / 50%';
 roundBorder.style.borderWidth = '2px';
 roundBorder.style.borderStyle = 'solid';
 roundBorder.style.borderColor = `${radarColor}`;
+roundBorder.style.opacity = '0.50';
 
 var squareBorder = hudRadar.FindChildTraverse('Radar__Square--Border');
 squareBorder.style.borderRadius = '0';
 squareBorder.style.borderWidth = '2px';
 squareBorder.style.borderStyle = 'solid';
 squareBorder.style.borderColor = `${radarColor}`;
+squareBorder.style.opacity = '0.50';
 
 var dashboardLabel = hudRadar.FindChildTraverse('DashboardLabel');
 dashboardLabel.style.color = `${dashboardLabelColor}`;
