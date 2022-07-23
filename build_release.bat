a02c0d0932400218c06d66083e0698864ecb1f4e
cristei <51248378+cristeigabriel@users.noreply.github.com>
2022-07-23 19:46:20 +0300

    oops

--- build_release.bat
+++ build_release.bat
@@ -1,7 +1,5 @@
 set PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.32.31326\bin\Hostx86\x86\
 
-cd C:\Users\gabriel\Documents\GitHub\cavalcade
-
 cmake.exe --preset x86-release
 cd out/build/x86-release/
 ninja -v
